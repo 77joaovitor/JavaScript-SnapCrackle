05a4d3e5e5d76c4e61219b964e234b241afdd766
77joaovitor <henrique.joaovitor@gmail.com>
2022-04-29 11:49:11 -0300

    completo

---
+++ snapCrackle.js
@@ -0,0 +1,15 @@
+function snapCrackle(n){
+    let array = []
+    for(let i = 1; i <= n; i++){
+        if(i % 2 != 0 && i % 5 == 0){
+            array.push("SnapCrackle")
+        }else if(i % 2 != 0){
+            array.push("Snap")
+        }else if(i % 5 == 0){
+            array.push("Crackle")
+        }else {
+            array.push(i)
+        }
+    }
+    return array.join(', ')
+}
